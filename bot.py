b04bff290a543564437ca5cb1bf6e48f005dde5b
jeffersoneirasoficial <jeffersoneirasoficial@yahoo.com>
2017-09-09 23:35:30 -0400

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot('276078996:AAG8jA0b5Z7DsW3wpNHTjlyy9UNShV3Tr_Q')
-
-@bot.message_hanler(commands=['start'])
-def send_wecolme(message):
-   bot.reply_to(message, "Olá bem-vindo ao Bot!")
-bot.polling()
